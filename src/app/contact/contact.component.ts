802e4bcc2d85f803b726939dc91a25393f047a74
rakeshtt92 <rakeshyasht92@gmail.com>
2026-03-28 20:46:50 +0530

    changes

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,9 +8,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
+
 export class ContactComponent {
   email    = 'rakeshyasht92@gmail.com';
-  phone    = '+46 761 509 115';
+  // phone    = '+46 761 509 115  | +91 9620156725';
   linkedin = 'https://linkedin.com/in/rakesh-tt';
   github   = 'https://github.com/rakesh-tt';
+
+  openEmail(): void {
+  window.open('https://mail.google.com/mail/?view=cm&to=rakeshyasht92@gmail.com', '_blank');
 }
+}
+
+
